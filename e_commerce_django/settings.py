9c95caa4a98a4fd02b52f271fb3a51c840eeda3d
moeendehqan <moeen.dehqan@gmail.com>
2025-10-05 23:12:52 +0330

    review header

--- e_commerce_django/settings.py
+++ e_commerce_django/settings.py
@@ -1,18 +1,18 @@
 import os
 from pathlib import Path
 AUTH_USER_MODEL = 'user.User'
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 AUTH_USER_MODEL = 'user.User'
 SECRET_KEY = 'django-insecure-l3#rjax53)!h!=q!m(y(r_=k)b(xmwvk@q3_8lo70d$j8f!6#)'
 
-# SECURITY WARNING: don't run with debug turned on in production!
+
 DEBUG = os.environ.get('DEBUG','False').lower() == 'true'
 
 ALLOWED_HOSTS = ['*']
-CSRF_TRUSTED_ORIGINS = os.environ.get("CSRF_TRUSTED_ORIGINS", "").split(",")
+CSRF_TRUSTED_ORIGINS = os.environ.get("CSRF_TRUSTED_ORIGINS", "https://shopsepid.ir,https://www.shopsepid.ir").split(",")
 SITE_ID = 1
 
 INSTALLED_APPS = [
@@ -71,24 +71,19 @@ TEMPLATES = [
 WSGI_APPLICATION = 'e_commerce_django.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get('POSTGRES_DB'),
-        'USER': os.environ.get('POSTGRES_USER'),
-        'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
-        'HOST': os.environ.get('POSTGRES_HOST'),
-        'PORT': os.environ.get('POSTGRES_PORT'),
+        'NAME': os.environ.get('POSTGRES_DB','sepidshop'),
+        'USER': os.environ.get('POSTGRES_USER','postgres'),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD','Moeen00006168'),
+        'HOST': os.environ.get('POSTGRES_HOST','193.56.135.20'),
+        'PORT': os.environ.get('POSTGRES_PORT','5432'),
     }
 }
 
 
-
-# Password validation
-# https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
     {
@@ -106,8 +101,6 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 
-# Internationalization
-# https://docs.djangoproject.com/en/4.2/topics/i18n/
 
 LANGUAGE_CODE = 'en-us'
 
@@ -118,8 +111,6 @@ USE_I18N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
@@ -140,7 +131,6 @@ JAZZMIN_UI_TWEAKS = {
 
 }
 
-# INSTALLED_APPS += ['ckeditor', 'ckeditor_uploader']
 CKEDITOR_UPLOAD_PATH = "uploads/"
 CKEDITOR_IMAGE_BACKEND = "pillow"
 CKEDITOR_CONFIGS = {
