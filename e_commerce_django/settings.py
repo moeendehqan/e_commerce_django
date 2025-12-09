2fe7f4c8c22fc18f0f231a2a66ba8ed8ab6a1aa9
moeendehqan <moeen.dehqan@gmail.com>
2025-12-09 23:26:36 +0330

    refactor: remove blog app and clean up unused media files
    Clean up project by removing the deprecated blog app including all its files (models, views, tests, etc.) and deleting unused media files that were taking up space. Also updated gitignore to properly exclude pycache and migrations.

--- e_commerce_django/settings.py
+++ e_commerce_django/settings.py
@@ -35,7 +35,6 @@ INSTALLED_APPS = [
     'product',
     'order',
     'core',
-    'blog',
 ]
 
 MIDDLEWARE = [
--- views.py
+++
@@ -1,7 +0,0 @@
-import logging
-logger = logging.getLogger(__name__)
-
-class ZarinpalCallbackView(View):
-    def get(self, request):
-        logger.info("درخواست بازگشت از زرین‌پال دریافت شد")
-        # ... کد موجود ...
